845e2586f79f3968588a3e7f55ff9f5f86b2e861
Evgeniy Evdokimov <evdokimov.ek@gmail.com>
2021-10-19 22:12:01 +0300

    Initial commit

---
+++ scoreboard.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Arial", 20, "normal")
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.goto(0, 275)
+        self.update()
+
+    def update(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
